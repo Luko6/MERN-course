9b5c81b1b6e1c80da0dd9624db3a3345a267ff02
Márk Lukovics <mark.lukovics@lombiq.com>
2022-05-07 20:09:03 +0200

    Styling

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users works!</h2>;
+  const DANNY =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Danny_DeVito_by_Gage_Skidmore.jpg/1200px-Danny_DeVito_by_Gage_Skidmore.jpg";
+
+  const USERS = [
+    { id: "u1", image: DANNY, name: "John Doe", places: 6 },
+    { id: "u2", image: DANNY, name: "Danny Doe", places: 9 },
+    { id: "u3", image: DANNY, name: "Bobby Doe", places: 1 },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
