3afa37314d5050dae547e2f7e878283b402550e1
Márk Lukovics <mark.lukovics@lombiq.com>
2022-05-07 19:42:09 +0200

    Adding basics of routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import NewPlace from "./places/pages/Place";
+import Users from "./user/pages/Users";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Users />} />
+        <Route exact path="/places/new" element={<NewPlace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
